7665053049fd074f09260712aedb9f9111983fa9
Chayel Heinsen <chayel@chayelheinsen.com>
2018-08-17 19:57:32 -0400

    Pass user to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def direct_to_dashboard
     redirect_to dashboard_index_path
   end
+
+  def serialized_user
+    UserSerializer.new(current_user).serializable_hash
+  end
+  helper_method :serialized_user
 end
